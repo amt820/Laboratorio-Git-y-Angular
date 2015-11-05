bc9489143c99909aa4cdbff16a263bbac07a2fe7
amt820 <amt820@hotmail.com>
2015-11-05 11:27:17 -0500

    Parte 2

---
+++ src/main/resources/static/js/app/controllers/$http.js
@@ -0,0 +1,20 @@
+var app = angular.module("app",[]);
+
+app.controller("blogCtrl", function($scope,$log,$http) {
+    $scope.entry = {title : "Title",
+                    content : "Content"};
+    $scope.entries = [];
+    $log.debug('se creo el $scope');
+
+    $scope.loadData = function() {
+        $http({
+            method: "GET",
+            url: "blogs"
+        }).success(function(data) {
+            $scope.entries = data;
+        }).error(function(data,status,headers,config) {
+            alert("Ha fallado la petición. Estado HTTP:"+status);
+        });
+    }
+    $scope.loadData();
+});
